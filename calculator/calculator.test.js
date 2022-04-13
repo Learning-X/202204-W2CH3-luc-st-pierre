a34f709619d61e7501092dff8e90c4f2c1f5f2c5
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-13 22:10:56 +0200

    Added the (arrange - act - assert)

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -2,6 +2,14 @@ const sum = (a, b) => a + b;
 
 describe("Given the function sum", () => {
   describe("When two numbers are added together", () => {
-    test("Then it should return the sum of those two numbers", () => {});
+    test("Then it should return the sum of those two numbers", () => {
+      const num1 = 8;
+      const num2 = 8;
+      const expectedResult = 16;
+
+      const result = sum(num1, num2);
+
+      expect(result).toBe(expectedResult);
+    });
   });
 });
