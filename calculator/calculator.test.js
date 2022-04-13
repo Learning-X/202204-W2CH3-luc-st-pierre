9fa96591b2249cbfe410b40080c257ebed8fa1b3
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-13 22:04:41 +0200

    Added sum function and started describing thetest

---
+++ calculator/calculator.test.js
@@ -0,0 +1,7 @@
+const sum = (a, b) => a + b;
+
+describe("Given the function sum", () => {
+  describe("When two numbers are added together", () => {
+    test("Then it should return the sum of those two numbers", () => {});
+  });
+});
