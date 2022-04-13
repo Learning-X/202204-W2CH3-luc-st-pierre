341fd724c6f84071524e78c0c774e3d86d455bdd
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-04-13 22:18:33 +0200

    Added the rest funtion and adjusted the (Arrange - act - assert)

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -1,4 +1,5 @@
 const sum = (a, b) => a + b;
+const rest = (a, b) => a - b;
 
 describe("Given the function sum", () => {
   describe("When two numbers are added together", () => {
@@ -8,6 +9,18 @@ describe("Given the function sum", () => {
       const expectedResult = 16;
 
       const result = sum(num1, num2);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+
+  describe("When two numbers are subtracting", () => {
+    test("Then it should return the difference", () => {
+      const num1 = 10;
+      const num2 = 5;
+      const expectedResult = 5;
+
+      const result = rest(num1, num2);
 
       expect(result).toBe(expectedResult);
     });
